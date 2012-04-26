017a591c33fd5ea9223c4463c0b0425978195b0a
nanomoomug <nano@moomug.com>
2012-04-26 21:37:49 +0200

    Move all executed code into a main.

--- series.py
+++ series.py
@@ -3,10 +3,6 @@ import os
 import sys
 import dircache
 
-HOME = os.environ['HOME']
-INSTALLATION_FOLDER = HOME + '/.series'
-DIRECTORY_FILE = INSTALLATION_FOLDER + '/directory.txt'
-
 def install():
     print 'Generating configuration file...'
     HOME = os.environ['HOME']
@@ -19,22 +15,6 @@ def install():
     programtxt.write( program.strip() )
     print 'Preference saved'
     return
-
-if not os.path.exists(INSTALLATION_FOLDER) or (len(sys.argv) > 1 and (sys.argv[1] == '-i' or sys.argv[1] == '--install')):
-    if len(sys.argv) > 2:
-        print 'Too many arguments. Use \'--help\' for help.'
-        exit()
-    install()
-    exit()
-
-if not os.path.exists(DIRECTORY_FILE):
-    videosDirectory = raw_input( 'No directory with videos given. Type directory with the videos:' )
-    DIRECTORY_FILE = open(DIRECTORY_FILE, 'w')
-    DIRECTORY_FILE.write( os.path.abspath(videosDirectory))
-
-DIRECTORY_FILE = open(DIRECTORY_FILE, 'r')
-directory = DIRECTORY_FILE.readline().strip()
-lastchaptertxt = directory + '/lastchapter.txt'
 
 if len(sys.argv) > 1:
     if ( sys.argv[1] == '-r' or sys.argv[1] == '--restart' ) and os.path.exists( './lastchapter.txt' ):
@@ -97,52 +77,76 @@ if len(sys.argv) > 1:
         print '-h --help         \t Prints this message.'
         exit()
 
-print "Searching for Next Chapter..."
-
-# Try to load the program that is to be used. First it looks for a
-# 'program.txt' file in the folder where series was called, if not
-# found is searches for '$HOME/.series/program.txt'.  If the
-# configuration file '$HOME/.series/program.txt' does not exist it
-# asks the user what program he wants to use and creates this file.
-if os.path.exists( './program.txt' ):
-    program = file( './program.txt' )
-else:
-    program = os.environ['HOME'] + '/.series/program.txt'
-    if not os.path.exists( program ):
+if __name__ == '__main__':
+    HOME = os.environ['HOME']
+    INSTALLATION_FOLDER = HOME + '/.series'
+    DIRECTORY_FILE = INSTALLATION_FOLDER + '/directory.txt'
+    
+    if not os.path.exists(INSTALLATION_FOLDER) or (len(sys.argv) > 1 and (sys.argv[1] == '-i' or sys.argv[1] == '--install')):
+        if len(sys.argv) > 2:
+            print 'Too many arguments. Use \'--help\' for help.'
+            exit()
         install()
-    program = file( program )
-program = program.readline().strip()
+        exit()
 
-if not os.path.exists( directory + '/lastchapter.txt' ):
-    newFile =  open(directory + '/lastchapter.txt', 'w' )
-    newFile.write( '0' )
-    newFile.close()
+    if not os.path.exists(DIRECTORY_FILE):
+        videosDirectory = raw_input( 'No directory with videos given. Type directory with the videos:' )
+        DIRECTORY_FILE = open(DIRECTORY_FILE, 'w')
+        DIRECTORY_FILE.write( os.path.abspath(videosDirectory))
+
+    DIRECTORY_FILE = open(DIRECTORY_FILE, 'r')
+    directory = DIRECTORY_FILE.readline().strip()
+    lastchaptertxt = directory + '/lastchapter.txt'
+    
+    print "Searching for Next Chapter..."
+
+    # Try to load the program that is to be used. First it looks for a
+    # 'program.txt' file in the folder where series was called, if not
+    # found is searches for '$HOME/.series/program.txt'.  If the
+    # configuration file '$HOME/.series/program.txt' does not exist it
+    # asks the user what program he wants to use and creates this file.
+    if os.path.exists( './program.txt' ):
+        program = file( './program.txt' )
+    else:
+        program = os.environ['HOME'] + '/.series/program.txt'
+        if not os.path.exists( program ):
+            install()
+        program = file( program )
+    program = program.readline().strip()
+
+    if not os.path.exists( directory + '/lastchapter.txt' ):
+        newFile =  open(directory + '/lastchapter.txt', 'w' )
+        newFile.write( '0' )
+        newFile.close()
 
 
-content = dircache.listdir(directory)
+    content = dircache.listdir(directory)
 
-def suffix( extension ):
-    def f(x): return x.endswith(extension)
-    return f
+    def suffix( extension ):
+        def f(x): return x.endswith(extension)
+        return f
 
-extensions = ['.avi','.mpg','.mpeg','.ogg','.ogm','.mkv']
+    extensions = ['.avi','.mpg','.mpeg','.ogg','.ogm','.mkv']
 
-videos = []
-for i in extensions:
-    videos.extend( filter( suffix(i) , content ) )
+    videos = []
+    for i in extensions:
+        videos.extend( filter( suffix(i) , content ) )
 
-videos = sorted(videos)
+    videos = sorted(videos)
 
-lastchapter = file( directory + '/lastchapter.txt' )
-chapter = lastchapter.readline()
+    lastchapter = file( directory + '/lastchapter.txt' )
+    chapter = lastchapter.readline()
 
-chapter = int(chapter)
-if chapter < len(videos):
-    newFile =  open(directory + '/lastchapter.txt', 'w' )
-    newFile.write( str(chapter + 1) )
-    newFile.close()
+    chapter = int(chapter)
+    if chapter < len(videos):
+        newFile =  open(directory + '/lastchapter.txt', 'w' )
+        newFile.write( str(chapter + 1) )
+        newFile.close()
 
-    print program + ' \"' + directory + '/' + videos[chapter] + '\"'
-    os.system(program + ' \"' + directory + '/' + videos[chapter] + '\"')
-else:
+        print program + ' \"' + directory + '/' + videos[chapter] + '\"'
+        os.system(program + ' \"' + directory + '/' + videos[chapter] + '\"')
+    else:
     print "No more chapters left :("
+
+
+    
